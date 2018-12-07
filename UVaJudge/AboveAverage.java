081e1e96372d6cb233357ebfce77ba15dc334b59
Kliver Daniel <kliver.giron1@correo.icesi.edu.co>
2018-12-06 20:50:45 -0500

    UVa Online Judge

--- UVaJudge/AboveAverage.java
+++ UVaJudge/AboveAverage.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
 /**
  * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1311
  * @author Kliver Daniel
